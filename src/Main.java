22361447be6bb6ce9b53dd2107bbcc9d493d5788
mariuszeq <igormajchrzak6b@gmail.com>
2024-09-12 10:37:07 +0200

    wypełnianie tablicy losowymi elementami

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,16 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+        //tablica- musi mieć rozmiar zdefiniowany jak w c++
+        int [] tablicaliczblosowych= new int [6];
+        for(int i = 0;i< tablicaliczblosowych.length;i++){
+            tablicaliczblosowych[i]= (int) (Math.random() * 100 + 1);
+        }
+        //wypisywanie tablicy
+        for(int elem : tablicaliczblosowych){
+            System.out.println(elem + " ");
+        }
+
+
     }
 }
