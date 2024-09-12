9a3d0ebbfcc0389a53a3eb6227498b3d027f9e80
mariuszeq <igormajchrzak6b@gmail.com>
2024-09-12 11:29:38 +0200

    Wpisywanie liczb z klawiatury

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,19 @@
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static <list> void main(String[] args) {
 
         System.out.println("Hello world!");
         //tablica- musi mieć rozmiar zdefiniowany jak w c++
-        int [] tablicaliczblosowych= new int [6];
-        for(int i = 0;i< tablicaliczblosowych.length;i++){
-            tablicaliczblosowych[i]= (int) (Math.random() * 100 + 1);
+        int[] tablicaliczblosowych = new int[6];
+        for (int i = 0; i < tablicaliczblosowych.length; i++) {
+            tablicaliczblosowych[i] = (int) (Math.random() * 100 + 1);
         }
         //wypisywanie tablicy
-        for(int elem : tablicaliczblosowych){
+        for (int elem : tablicaliczblosowych) {
             System.out.println(elem + " ");
         }
         //wypełnianie kolekcji wartościami losowymi
@@ -23,7 +24,7 @@ public class Main {
         //map ->
         List<Integer> Listaliczblosowych = new ArrayList<>();
         for (int i = 0; i < 6; i++) {
-            int liczba = (int) (Math.random() * 100 +1);
+            int liczba = (int) (Math.random() * 100 + 1);
             Listaliczblosowych.add(liczba);
         }
         System.out.println("Wylosowana lista: ");
@@ -32,27 +33,38 @@ public class Main {
         }
         System.out.println(Listaliczblosowych);
         //losowanie bez powt
-        List <Integer> listalosowychbezpowt = new ArrayList<>();
+        List<Integer> listalosowychbezpowt = new ArrayList<>();
         int liczba;
+
         for (int i = 0; i < 6; i++) {
             liczba = (int) (Math.random() * 100 + 1);
-            while (listalosowychbezpowt.contains(liczba)){
+            while (listalosowychbezpowt.contains(liczba)) {
                 liczba = (int) (Math.random() * 100 + 1);     //zrobić to!!!
             }
             System.out.println(liczba);
         }
         /*
-        * lista - indeksowana, uporządkowana, elementy nieunikatowe
-        * zbiór - nieideksowany(zazwyczaj), nieuporządkowana(zazwyczaj), elementy unikatowe
-        * */
+         * lista - indeksowana, uporządkowana, elementy nieunikatowe
+         * zbiór - nieideksowany(zazwyczaj), nieuporządkowana(zazwyczaj), elementy unikatowe
+         * */
 
         // zbiór - też zawsze typu złożonego
-        HashSet <Integer> zbior = new HashSet<>();
-        while(zbior.size()< 6){
+        HashSet<Integer> zbior = new HashSet<>();
+        while (zbior.size() < 6) {
             liczba = (int) (Math.random() * 100 + 1);
             zbior.add(liczba);
         }
         System.out.println("Zbiór: ");
         System.out.println(zbior);
+        //lista liczb wczytywanych z klawiatury
+        Scanner klawiatura = new Scanner(System.in);
+        List<Integer> listaklawiatura = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            System.out.println("Wpisz liczbę z klawiatury");
+            int wpisanaliczba = klawiatura.nextInt();
+            listaklawiatura.add(wpisanaliczba);
+
+        }
+        System.out.println(listaklawiatura);
     }
 }
